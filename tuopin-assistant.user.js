b2ddade870ea35762b4dc9c52d9962d0dbc845e9
handingdong4-ship-it <handingdong4@gmail.com>
2026-06-30 10:49:59 +0800

    Bump version to 1.8.2

--- tuopin-assistant.user.js
+++ tuopin-assistant.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         大淘客拓品助手
 // @namespace    https://www.dataoke.com/
-// @version      1.8.1
+// @version      1.8.2
 // @downloadURL  https://raw.githubusercontent.com/handingdong4-ship-it/tuopin-assistant/main/tuopin-assistant.user.js
 // @updateURL    https://raw.githubusercontent.com/handingdong4-ship-it/tuopin-assistant/main/tuopin-assistant.user.js
 // @description  在大淘客选品库页面，商品卡片左上角显示复选框，勾选即选中，配合浮动工具栏获取商品详情及优惠文案，支持一键发布到SMZDM
